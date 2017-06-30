c223ff84fe04aada23158cfcea68a6a21691fc3c
maksimmarchyk <maksim-nitka2015@yandex.ru>
2017-06-30 08:34:45 +0300

    максим

---
+++ 3WPF/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _3WPF
+{ //разработайте программу на wpf. создайте два разных стиля для кнопок. по нажатию на кнопку применение стиля должно циклически меняться
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// //Проба
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
